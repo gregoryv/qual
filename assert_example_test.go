f78f2d7ca0c3bf60f4d865a471e155087db557b7
Gregory Vincic <g@7de.se>
2018-08-19 09:15:35 +0200

    Add test to get feel for request/response testing

--- assert_example_test.go
+++ assert_example_test.go
@@ -1,20 +1,23 @@
 package qual
 
 import (
+	"net/http"
 	"os"
 )
 
-func ExampleAssert_missing_source_code() {
+// When using the Assert func even though you do not have access to the
+// source code, eg. when doing integration tests a generic output will be given.
+func ExampleAssert_missingSourceCode() {
 	os.Rename("assert_example_test.go", "missing.go")
 	defer os.Rename("missing.go", "assert_example_test.go")
 
-	msg := "Eg. integration test"
-	result := make([]int, 0)
-	Assert(t, Vars{len(result), msg},
-		len(result) == 1,
+	msg := "Verify response status code"
+	resp, _ := http.Get("http://www.example.com/existing_file")
+	Assert(t, Vars{resp.StatusCode, msg},
+		resp.StatusCode == 200,
 	)
 	// Output:
 	//   failed assert[0]
-	//     Vars[0] = 0
-	//     Vars[1] = "Eg. integration test"
+	//     Vars[0] = 404
+	//     Vars[1] = "Verify response status code"
 }
