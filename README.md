d21ca07394dba0ea3217a6fd2e42a25368c75a98
Gregory Vincic <g@7de.se>
2018-12-15 09:35:26 +0100

    Fix coverage

--- README.md
+++ README.md
@@ -49,6 +49,6 @@ documentation.
 
 For a period this package came with an Assert func that parsed code to
 create presentable error messages. This proved to be less readable
-than intended. However minimizing complexity by removing one pathway
-is still provided with
+than intended(available in 0.2.1). However minimizing complexity by
+removing one pathway is still provided with
 the [Wrap](https://godoc.org/github.com/gregoryv/qual#Wrap) func.
--- wrap_test.go
+++ wrap_test.go
@@ -8,7 +8,20 @@ import (
 func TestWrap(t *testing.T) {
 	If := Wrap(t)
 	If(false).Log("Not ok")
-	If(true).Log("Ok")
+	If(true).Logf("%v", true)
+
+	If = Wrap(&noopT{})
+	If(true).Error()
+	If(true).Errorf("%s", "yes")
+	If(true).Fatal()
+	If(true).Fatalf("%s", "yes")
+	If(true).Log()
+	If(true).Logf("%s %s", "yes", "no")
+	If(true).Fail()
+	If(true).FailNow()
+	If(true).Skip()
+	If(true).SkipNow()
+	If(true).Skipf("%s %s", "yes", "no")
 }
 
 var t *noopT = &noopT{} // mock for *testing.T
