285ae9c9774a45846f241f185b03c0dcc478a9f1
Gregory Vincic <g@7de.se>
2018-12-15 09:26:30 +0100

    Test assert func

--- wrap_test.go
+++ wrap_test.go
@@ -1,6 +1,15 @@
 package qual
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWrap(t *testing.T) {
+	If := Wrap(t)
+	If(false).Log("Not ok")
+	If(true).Log("Ok")
+}
 
 var t *noopT = &noopT{} // mock for *testing.T
 var err error = fmt.Errorf("")
