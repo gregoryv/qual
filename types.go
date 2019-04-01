4544b9f525e5d927b7212865b0d325754fe669eb
Gregory Vincic <g@7de.se>
2019-04-01 21:59:58 +0200

    Tidy type T

--- types.go
+++ types.go
@@ -4,19 +4,10 @@ type T interface {
 	Helper()
 	Error(...interface{})
 	Errorf(string, ...interface{})
-	Fatal(...interface{})
-	Fatalf(string, ...interface{})
-	Fail()
-	FailNow()
 }
 
 type noopT struct{}
 
-func (t *noopT) Helper()                                    {}
-func (t *noopT) Error(...interface{})                       {}
-func (t *noopT) Errorf(string, ...interface{})              {}
-func (t *noopT) Fatal(...interface{})                       {}
-func (t *noopT) Fatalf(string, ...interface{})              {}
-func (t *noopT) Fail()                                      {}
-func (t *noopT) FailNow()                                   {}
-func (t *noopT) Equals(got, exp interface{}, msg ...string) {}
+func (t *noopT) Helper()                       {}
+func (t *noopT) Error(...interface{})          {}
+func (t *noopT) Errorf(string, ...interface{}) {}
